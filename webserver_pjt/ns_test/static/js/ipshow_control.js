06778c7738018032c56150d438ff3256416633e6
myname <my@example.com>
2020-05-02 00:11:15 +0800

    fun add

--- webserver_pjt/ns_test/static/js/ipshow_control.js
+++ webserver_pjt/ns_test/static/js/ipshow_control.js
@@ -8,17 +8,17 @@ function swColor(){
 		}else if(sps[i].getAttribute("name") == "0"){
 			// sps[i].style.backgroundColor="#FF0000";
 			sps[i].style.backgroundColor="rgba(255,0,0,0.8)";
-		}else if(sps[i].getAttribute("name") == "3"){
+		}else if(sps[i].getAttribute("name") == "2"){
 			// sps[i].style.backgroundColor="#FF0000";
 			sps[i].style.backgroundColor="rgba(255,150,0,1)";
 		}else{
 			sps[i].style.setProperty('background-color','#DCDCDC');
-			sps[i].style.color = "red";
-			sps[i].style.fontSize = "18px";
+			sps[i].style.color = "#FF4500";
+			sps[i].style.fontSize = "25px";
 			sps[i].style.fontFamily = "宋体";
 			sps[i].style.textAlign = "center";
-			sps[i].style.lineHeight = "12px";
-			sps[i].innerHTML = "!";
+			sps[i].style.lineHeight = "13px";
+			sps[i].innerHTML = "⚡︎";
 		}
 	}
 	setTimeout("rmStyle()", 1000);
@@ -36,4 +36,7 @@ function begin(){
 	var _switch=setInterval("swColor()", 2000);
 }
 begin();
+function startipt(obj){
+	alert("添加备注的功能正在开发中，敬请期待");
+}
 // swColor();
