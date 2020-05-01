592004ce826695de624c8385c216a75c1c66fcbb
myname <my@example.com>
2020-05-01 21:27:37 +0800

    html modify

--- webserver_pjt/ns_test/static/js/ipshow_control.js
+++ webserver_pjt/ns_test/static/js/ipshow_control.js
@@ -1,5 +1,5 @@
 function swColor(){
-	var sps = document.getElementsByClassName('spsty');
+	var sps = document.getElementsByClassName("spsty");
 	for(var i=0;i<sps.length;i++)
 	{	
 		if(sps[i].getAttribute("name") == "1"){
@@ -8,12 +8,23 @@ function swColor(){
 		}else if(sps[i].getAttribute("name") == "0"){
 			// sps[i].style.backgroundColor="#FF0000";
 			sps[i].style.backgroundColor="rgba(255,0,0,0.8)";
+		}else if(sps[i].getAttribute("name") == "3"){
+			// sps[i].style.backgroundColor="#FF0000";
+			sps[i].style.backgroundColor="rgba(255,150,0,1)";
+		}else{
+			sps[i].style.setProperty('background-color','#DCDCDC');
+			sps[i].style.color = "red";
+			sps[i].style.fontSize = "18px";
+			sps[i].style.fontFamily = "宋体";
+			sps[i].style.textAlign = "center";
+			sps[i].style.lineHeight = "12px";
+			sps[i].innerHTML = "!";
 		}
 	}
 	setTimeout("rmStyle()", 1000);
 }
 function rmStyle(){
-	var sps = document.getElementsByClassName('spsty');
+	var sps = document.getElementsByClassName("spsty");
 	for(var i=0;i<sps.length;i++)
 	{
 		if(sps[i].getAttribute("name") == "1"){
