89eae6ffe1508a73d77c1ca12b104f0b28c2a448
myname <my@example.com>
2020-05-04 23:19:23 +0800

    gui 80%

--- operate_gui.py
+++ operate_gui.py
@@ -2,10 +2,16 @@ import tkinter
 from tkinter import Label, Button, StringVar, messagebox, simpledialog
 from tkinter.messagebox import *
 import subprocess
+import os
+import time
+
+
+MGR_FILE = "manage.py"
+MGR_DIR = "webserver_pjt"
+MGR_PATH = os.path.join(MGR_DIR, MGR_FILE)
 
 
 root = tkinter.Tk()
-# print(dir(root))
 setWidth, setHeight = root.maxsize()
 root.geometry('320x190+%d+%d' % ((setWidth-320)/2, (setHeight)/2-190))
 root.title('运行助手')
@@ -21,53 +27,45 @@ def change_text():
 		button1['background'] = "#32A084"
 		messagebox.showinfo(title='提示信息', message='开始运行')
 	else:
-		if askyesno("操作提示", "是否停止服务？", default="no"):
+		if askyesno('操作提示', '是否停止服务？', default='no'):
 			button1['text'] = "运行"
 			button1['background'] = "#EBEDEF"
 
 
+def run_mgrshell(run_tp):
+	mark = time.strftime("RA+%Y%m%d %H:%M:%S", time.localtime())
+	cmd = 'start run_assistant.bat "%s" %s' % (mark, run_tp)
+	_bat = subprocess.Popen(cmd, shell=True)
+	print()
+	button1.config(state=tkinter.DISABLED)
+	button2.config(state=tkinter.DISABLED)
+	button3.config(state=tkinter.DISABLED)
+	root.update()
+	_bat.wait()
+	while True:
+		task_info = subprocess.Popen('tasklist /V | findstr /C:"%s"' % mark, shell=True, stdout=subprocess.PIPE)
+		if not task_info.stdout.read():
+		    button1.config(state=tkinter.ACTIVE)
+		    button2.config(state=tkinter.ACTIVE)
+		    button3.config(state=tkinter.ACTIVE)
+		    break
 
 
-
-# print(dir(bvar1))
-label1 = Label(text='web服务',width=25,borderwidth=2,relief="groove",background="#f60",foreground="white")
-print(dir(label1))
-'''
-grid_configure(cnf={}, **kw) method of tkinter.Label instance
-    Position a widget in the parent widget in a grid. Use as options:
-    column=number - use cell identified with given column (starting with 0)
-    columnspan=number - this widget will span several columns
-    in=master - use master to contain this widget
-    in_=master - see 'in' option description
-    ipadx=amount - add internal padding in x direction
-    ipady=amount - add internal padding in y direction
-    padx=amount - add padding in x direction
-    pady=amount - add padding in y direction
-    row=number - use cell identified with given row (starting with 0)
-    rowspan=number - this widget will span several rows
-    sticky=NSEW - if cell is larger on which sides will this
-                  widget stick to the cell boundary
-'''
+label1 = Label(text='web服务',width=25,borderwidth=2,relief='groove',background='#f60',foreground='white')
 button1 = Button(text='运行',background="#EBEDEF",command=change_text)
 label1.grid(row=0,column=0,padx=15,pady=10,ipadx=5,ipady=6)
 button1.grid(row=0,column=5,padx=30,pady=10,ipadx=5,ipady=2)
 
-label2 = Label(text='管理终端',width=25,borderwidth=2,relief="groove",background="#f60",foreground="white")
-button2 = Button(text='运行',background="#EBEDEF")
+label2 = Label(text='管理终端',width=25,borderwidth=2,relief='groove',background='#f60',foreground='white')
+button2 = Button(text='运行',background='#EBEDEF',command=lambda:run_mgrshell('python manage.py shell'))
 label2.grid(row=1,column=0,padx=15,pady=10,ipadx=5,ipady=6)
 button2.grid(row=1,column=5,padx=30,pady=10,ipadx=5,ipady=2)
 
-label3 = Label(text='数据库终端',width=25,borderwidth=2,relief="groove",background="#f60",foreground="white")
-button3 = Button(text='运行',background="#EBEDEF")
+label3 = Label(text='数据库终端',width=25,borderwidth=2,relief='groove',background='#f60',foreground='white')
+button3 = Button(text='运行',background="#EBEDEF",command=lambda:run_mgrshell('python manage.py dbshell'))
 label3.grid(row=3,column=0,padx=15,pady=10,ipadx=5,ipady=6)
 button3.grid(row=3,column=5,padx=30,pady=10,ipadx=5,ipady=2)
 
-# bottom_button1 = Button(text='口令1')
-# bottom_button1.grid(row=4, column=0)
-# bottom_button2 = Button(text='口令1')
-# bottom_button2.grid(row=4, column=1)
-# bottom_button3 = Button(text='口令1')
-# bottom_button3.grid(row=4, column=2)
 
 if __name__ == '__main__':
 	root.mainloop()
---
+++ run_assistant.bat
@@ -0,0 +1,5 @@
+@echo off
+cd "C:\Users\Administrator\Desktop\auto_nes_test\pyenv\Scripts"
+call activate.bat 
+cd "..\..\outter_codes\webserver_pjt"
+%2 %3 %4
