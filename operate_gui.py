4e57284943a34f6c1494cb669e3ea90926b7bfda
myname <my@example.com>
2020-05-05 00:15:11 +0800

    up 0:15

--- operate_gui.py
+++ operate_gui.py
@@ -13,7 +13,7 @@ MGR_PATH = os.path.join(MGR_DIR, MGR_FILE)
 
 root = tkinter.Tk()
 setWidth, setHeight = root.maxsize()
-root.geometry('320x190+%d+%d' % ((setWidth-320)/2, (setHeight)/2-190))
+root.geometry('320x210+%d+%d' % ((setWidth-320)/2, (setHeight)/2-210))
 root.title('运行助手')
 root.resizable(width=False, height=False)
 
@@ -51,20 +51,29 @@ def run_mgrshell(run_tp):
 		    break
 
 
+def open_explo(url):
+	subprocess.run('chrome %s' % url)
+
+
 label1 = Label(text='web服务',width=25,borderwidth=2,relief='groove',background='#f60',foreground='white')
 button1 = Button(text='运行',background="#EBEDEF",command=change_text)
-label1.grid(row=0,column=0,padx=15,pady=10,ipadx=5,ipady=6)
+label1.grid(row=0,column=0,columnspan=5,padx=15,pady=10,ipadx=5,ipady=6)
 button1.grid(row=0,column=5,padx=30,pady=10,ipadx=5,ipady=2)
 
 label2 = Label(text='管理终端',width=25,borderwidth=2,relief='groove',background='#f60',foreground='white')
 button2 = Button(text='运行',background='#EBEDEF',command=lambda:run_mgrshell('python manage.py shell'))
-label2.grid(row=1,column=0,padx=15,pady=10,ipadx=5,ipady=6)
+label2.grid(row=1,column=0,columnspan=5,padx=15,pady=10,ipadx=5,ipady=6)
 button2.grid(row=1,column=5,padx=30,pady=10,ipadx=5,ipady=2)
 
 label3 = Label(text='数据库终端',width=25,borderwidth=2,relief='groove',background='#f60',foreground='white')
 button3 = Button(text='运行',background="#EBEDEF",command=lambda:run_mgrshell('python manage.py dbshell'))
-label3.grid(row=3,column=0,padx=15,pady=10,ipadx=5,ipady=6)
+label3.grid(row=3,column=0,columnspan=5,padx=15,pady=10,ipadx=5,ipady=6)
 button3.grid(row=3,column=5,padx=30,pady=10,ipadx=5,ipady=2)
+
+button_index = Button(text='首页',command=lambda:open_explo('127.0.0.1:8000/index'))
+button_index.grid(row=4,column=3,padx=10,ipadx=5,ipady=2)
+button_admin = Button(text='控制台',command=lambda:open_explo('127.0.0.1:8000/admin'))
+button_admin.grid(row=4,column=4,ipady=2)
 
 
 if __name__ == '__main__':
