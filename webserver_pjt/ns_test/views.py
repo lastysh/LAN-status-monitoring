c28afdc7f28f62b1cff3fb76c79e3a42106a8e39
meme <example@example.com>
2020-06-01 17:48:01 +0800

    update button in html

--- webserver_pjt/ns_test/views.py
+++ webserver_pjt/ns_test/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from ns_test import models
 # Create your views here.
 
-# import sys
-# sys.path.append(r"../..")
+import sys
+sys.path.append(r"../..")
 # from network_status_test import *
 
 def test_index(request):
@@ -12,8 +12,10 @@ def test_index(request):
 		ip_list = models.Ips.objects.all()
 		return render(request, 'ipshow/index.html', {'baidu': ip_list[0], 'ip_list': ip_list[1:]})
 
+
 def index(request):
 	return HttpResponseRedirect('/')
+
 
 def login(request):
 	return HttpResponse("<b>功能正在开发中，敬请期待</b>")
@@ -21,3 +23,7 @@ def login(request):
 
 def register(request):
 	return HttpResponse("<b>功能正在开发中，敬请期待</b>")
+
+
+def update_state(request):
+	pass
