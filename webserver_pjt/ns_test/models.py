a5287aa5018d680123bd40b0d9aa16925cc564c9
meme <example@example.com>
2020-06-15 18:07:33 +0800

    add table comment

--- webserver_pjt/ns_test/models.py
+++ webserver_pjt/ns_test/models.py
@@ -24,3 +24,16 @@ class Ips(models.Model):
 
     def __str__(self):
         return self.ip
+
+
+class Comment(models.Model):
+    ip = models.CharField(verbose_name='IP地址', max_length=128, unique=True, editable=False)
+    comment = models.CharField(verbose_name='备注', max_length=128, blank=True)
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = "备注"
+        verbose_name_plural = "备注列表"
+
+    def __str__(self):
+        return self.ip
