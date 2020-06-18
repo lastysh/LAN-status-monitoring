81e4a1c0e38ff548e21ca47b0d7f0c29bffd6bdf
myname <my@example.com>
2020-06-19 07:53:01 +0800

    2020/6/19 commit

--- webserver_pjt/ns_test/admin.py
+++ webserver_pjt/ns_test/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from ns_test.models import Ips
+from ns_test.models import Ips, Comment
 
 # Register your models here.
 class IpsAdmin(admin.ModelAdmin):
 	list_display = ['ip', 'mac', 'name', 'status', 'comment']
 
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ['ip', 'comment']
+
 
 admin.site.register(Ips, IpsAdmin)
+admin.site.register(Comment, CommentAdmin)
